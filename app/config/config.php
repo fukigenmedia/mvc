8d17b5191cfe1ee47e391e29ba8c4827fbb38a4f
Mitra <47443465+mitra-a@users.noreply.github.com>
2023-12-01 23:24:12 +0800

    default config👌

--- app/config/config.php
+++ app/config/config.php
@@ -1,8 +1,8 @@
 <?php
 
-define('base_url', 'http://localhost/petiknews');
+define('base_url', 'http://localhost/mvc');
 
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
 define('DB_PASS', '');
-define('DB_NAME', 'petiknews');
+define('DB_NAME', '');
